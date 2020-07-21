31e5966b0f4a9ad528127c9bd3a9f4e55499b35c
Jordi Orriols <jorriols@miteksystems.com>
2020-07-21 13:58:25 +0200

    Add storybook-static

---
+++ storybook-static/precache-manifest.e7e073522065e04185271bec20c05714.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795257f4bf86ccef8bd1d78535dc0864",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.71f95514294f190cec57.bundle.js"
+  },
+  {
+    "url": "main.71f95514294f190cec57.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "runtime~main.71f95514294f190cec57.bundle.js"
+  },
+  {
+    "revision": "efc2d17545da0bed1146",
+    "url": "static/css/main.3f554f14.chunk.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "url": "vendors~main.71f95514294f190cec57.bundle.js"
+  },
+  {
+    "url": "vendors~main.71f95514294f190cec57.bundle.js.LICENSE.txt"
+  }
+]);
